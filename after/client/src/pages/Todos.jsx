cb9f37be3135809937f34a7772fedc807f411bca
Askia <anabassoua@gmail.com>
2023-12-15 22:28:26 -0500

    update

--- after/client/src/pages/Todos.jsx
+++ after/client/src/pages/Todos.jsx
@@ -8,13 +8,14 @@ function Todos() {
       <h1 className="page-title">Todos</h1>
       <ul>
         {todos.map((todo) => {
-          if (!todo.completed) return <li key={todo.id}>{todo.title}</li>;
-          else
-            return (
-              <li key={todo.id} className="strike-through">
-                {todo.title}
-              </li>
-            );
+          return (
+            <li
+              key={todo.id}
+              className={todo.completed ? "strike-through" : undefined}
+            >
+              {todo.title}
+            </li>
+          );
         })}
       </ul>
     </div>
